a9763bced66085af431bea6a20df33768cf75d57
xindy <xinpeng_lian@163.com>
2021-03-16 23:23:57 +0800

    0316_study(微信接口实战,Pytest)

--- test_requests/test_wework.py
+++ test_requests/test_wework.py
@@ -1,8 +1,18 @@
 # coding:  utf-8
 # @Author  : LianXinPeng
 # @Time    : 2021/3/15 21:45
+import random
+import re
+
 import pytest
 import requests
+
+
+def test_create_data():
+    """userid,name,mobile"""
+    # data = [(str(random.randint(0, 9999999)), "五月天", str(random.randint(13800000000, 13900000000))) for x in range(6)]
+    data = [("wuyue" + str(x), "五月天", "138%08d" % x) for x in range(10 )]
+    return data
 
 
 class TestWework():
@@ -23,7 +33,7 @@ class TestWework():
             "https://qyapi.weixin.qq.com/cgi-bin/gettoken", params=request_params)
         return r.json()['access_token']
 
-    def test_create(self, token, userid, mobile, name="五月天", department=None):
+    def test_create(self, token, userid, name, mobile, department=None):
         """
         请求方式：POST（HTTPS）
         请求地址：https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token=ACCESS_TOKEN
@@ -52,7 +62,7 @@ class TestWework():
             f"https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token={token}&userid={userid}")
         return user.json()
 
-    def test_edit_user(self, token, userid, mobile="13700000001", name="六月的雨"):
+    def test_edit_user(self, token, userid, name="六月的雨"):
         """
         请求方式：POST（HTTPS）
         请求地址：https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token=ACCESS_TOKEN
@@ -60,8 +70,7 @@ class TestWework():
         """
         request_body = {
             "userid": userid,
-            "name": name,
-            "mobile": mobile
+            "name": name
         }
         edit_user = requests.post(f"https://qyapi.weixin.qq.com/cgi-bin/user/update?access_token={token}",
                                   json=request_body)
@@ -76,12 +85,23 @@ class TestWework():
         user_id = "wuyuetian"
         user = requests.get(
             f"https://qyapi.weixin.qq.com/cgi-bin/user/delete?access_token={token}&userid={userid}")
-        print(user.json())
+        return user.json()
 
-    def test_wework(self, token):
+    @pytest.mark.parametrize("user_id, name, mobile", test_create_data())
+    def test_wework(self, token, user_id, name, mobile):
         """
         整体测试
         :return:
         """
-        self.test_create(token, "wuyuetian", "13799999991")
-        self.test_get_user(token, "wuyuetian")
+        try:
+            assert "created" == self.test_create(token, user_id, name, mobile)["errmsg"]
+        except AssertionError as e:
+            if "mobile existed" in e.__str__():
+                re_userid = re.findall(":(.*)'", e.__str__())[0]
+                self.test_delete_user(token, re_userid)
+                assert "created" == self.test_create(token, user_id, name, mobile)["errmsg"]
+        assert name == self.test_get_user(token, user_id)["name"]
+        assert "updated" == self.test_edit_user(token, user_id, name="八月的风")["errmsg"]
+        assert "八月的风" == self.test_get_user(token, user_id)["name"]
+        assert "deleted" == self.test_delete_user(token, user_id)["errmsg"]
+        assert 60111 == self.test_get_user(token, user_id)["errcode"]
